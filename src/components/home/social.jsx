69b8db151bca914e580d8cc39756bd01d739bd43
Summer-source42 <thisisnaushad@proton.me>
2024-01-13 05:02:05 +0530

    first commit

---
+++ src/components/home/social.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.instagram.com/thesource05?igsh=MTk2a2VsbzVzNzZ2aQ" className="home_social-icon" target="_blank"> 
+                <i className="uil uil-instagram"></i>
+            </a>
+
+            <a href="https://www.whatsapp.com/" className="home_social-icon" target="_blank">
+                <i className="uil uil-whatsapp"></i>
+            </a>
+
+            <a href="https://www.github.com/Summer-source42" className="home_social-icon" target="_blank"> 
+                <i className="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+export default Social
